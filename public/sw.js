8b33ba60622abe1709709ba9e2d6f557d9ca3da7
Ali Bulut <ali@alibulut.net>
2021-07-28 13:27:30 +0300

    retrieving items from the cache

--- public/sw.js
+++ public/sw.js
@@ -14,5 +14,16 @@ self.addEventListener("activate", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // if response is null, that means there is no cached version for the request. So use default fetch func.
+      // cache mechanism => key-value pair. Key is always request object and value is always the response. That's why
+      // we are using caches.match(event.request) that, if there is a key value pair in caches.
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    })
+  );
 });
