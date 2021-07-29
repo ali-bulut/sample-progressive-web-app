2840412f4df47494aef2ba9aa757012ceebc08e7
Ali Bulut <ali@alibulut.net>
2021-07-29 16:57:07 +0300

    requesting permission for notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,7 @@
 var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll(
+  ".enable-notifications"
+);
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(function () {
@@ -11,3 +14,23 @@ window.addEventListener("beforeinstallprompt", function (event) {
   deferredPrompt = event;
   return false;
 });
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function (result) {
+    console.log("User Choice", result);
+    if (result !== "granted") {
+      console.log("No notification permission granted!");
+    } else {
+    }
+  });
+}
+
+if ("Notification" in window) {
+  for (let i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = "inline-block";
+    enableNotificationsButtons[i].addEventListener(
+      "click",
+      askForNotificationPermission
+    );
+  }
+}
