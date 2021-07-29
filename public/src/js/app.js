e3af4c19b027e815fa368f9731f41f97d479f6aa
Ali Bulut <ali@alibulut.net>
2021-07-29 16:59:59 +0300

    displaying notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -15,12 +15,20 @@ window.addEventListener("beforeinstallprompt", function (event) {
   return false;
 });
 
+function displayConfirmNotification() {
+  var options = {
+    body: "You successfully subscribed to our Notification Service.",
+  };
+  new Notification("Successfully Subscribed!", options);
+}
+
 function askForNotificationPermission() {
   Notification.requestPermission(function (result) {
     console.log("User Choice", result);
     if (result !== "granted") {
       console.log("No notification permission granted!");
     } else {
+      displayConfirmNotification();
     }
   });
 }
