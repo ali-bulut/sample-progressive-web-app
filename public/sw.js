fdba66f9ffec456bb62fa4f32f97a06788d26022
Ali Bulut <ali@alibulut.net>
2021-07-29 17:22:08 +0300

    notificationclick event

--- public/sw.js
+++ public/sw.js
@@ -243,3 +243,17 @@ self.addEventListener("sync", function (event) {
     );
   }
 });
+
+self.addEventListener("notificationclick", function (event) {
+  var notification = event.notification;
+  var action = event.action;
+
+  console.log(notification);
+
+  if (action === "confirm") {
+    console.log("Confirm was chosen!");
+  } else {
+    console.log(action);
+  }
+  notification.close();
+});
