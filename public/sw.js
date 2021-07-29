f41905f194829f39040fd983f3abbe11d61a7578
Ali Bulut <ali@alibulut.net>
2021-07-29 14:01:12 +0300

    refactoring

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
 importScripts("/src/js/idb.js");
+importScripts("/src/js/utility.js");
 
 const STATIC_CACHE_VERSION = "static-v7"; // if we change anything in our project, we just need to update its version from here to update caches.
 const DYNAMIC_CACHE_VERSION = "dynamic-v4";
@@ -19,12 +20,6 @@ const STATIC_FILES = [
   "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
 ];
 // const MAX_CACHE_ITEM_COUNT = 20;
-
-let dbPromise = idb.open("posts-store", 1, function (db) {
-  if (!db.objectStoreNames.contains("posts")) {
-    db.createObjectStore("posts", { keyPath: "id" });
-  }
-});
 
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName).then(function (cache) {
@@ -157,12 +152,7 @@ self.addEventListener("fetch", function (event) {
         var clonedRes = res.clone();
         clonedRes.json().then(function (data) {
           for (var key in data) {
-            dbPromise.then(function (db) {
-              var tx = db.transaction("posts", "readwrite");
-              var store = tx.objectStore("posts");
-              store.put(data[key]);
-              return tx.complete;
-            });
+            writeData("posts", data[key]);
           }
         });
         return res;
