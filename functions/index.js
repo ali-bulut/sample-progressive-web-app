949dfe2af9902d7a2df243d90e7df0be0a31643d
Ali Bulut <ali@alibulut.net>
2021-07-29 16:33:55 +0300

    storePostData function added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
-const functions = require("firebase-functions");
+var functions = require("firebase-functions");
+var admin = require("firebase-admin");
+var cors = require("cors")({ origin: true });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var serviceAccount = require("./pwagram-firebase-pk.json");
+
+admin.initializeApp({
+  databaseURL: "https://u-pwagram-default-rtdb.firebaseio.com/",
+  credential: admin.credential.cert(serviceAccount),
+});
+
+exports.storePostData = functions.https.onRequest(function (request, response) {
+  cors(request, response, function () {
+    admin
+      .database()
+      .ref("posts")
+      .push({
+        id: request.body.id,
+        title: request.body.title,
+        location: request.body.location,
+        image: request.body.image,
+      })
+      .then(function () {
+        response
+          .status(201)
+          .json({ message: "Data Stored", id: request.body.id });
+      })
+      .catch(function (err) {
+        response.status(500).json({ error: err });
+      });
+  });
+});
